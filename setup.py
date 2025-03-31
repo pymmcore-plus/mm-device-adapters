002e340e9cfbdba97f871667c2371541a6631e1d
Talley Lambert <talley.lambert@gmail.com>
2025-03-31 11:51:51 -0400

    fix version

--- setup.py
+++ setup.py
@@ -7,23 +7,13 @@ from setuptools.dist import Distribution
 sys.path.append(".")
 from install import install
 
-
-class CustomEggInfo(egg_info):
-    def run(self):
-        div, release = install()
-        version = f"{div}.{release[:4]}.{release[4:6]}.{release[6:8]}"
-        self.distribution.metadata.version = version
-        return super().run()
+div, release = install()
+version = f"{div}.{release[:4]}.{release[4:6]}.{release[6:8]}"
 
 
 class BinaryDistribution(Distribution):
     def has_ext_modules(self) -> bool:
         return True  # Forces a platform-specific wheel
 
-    def get_command_class(self, command_name):
-        if command_name == "egg_info":
-            return CustomEggInfo
-        return super().get_command_class(command_name)
 
-
-setup(distclass=BinaryDistribution)
+setup(version=version, distclass=BinaryDistribution)
